f89c6c6fe089f422f6b192eb4056cec3e8533f86
[new transacton] <shashank.sai499@gmail.com>
2024-12-13 08:15:59 -0600

    Initial Commit

---
+++ Weather Tracker/Weather_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TrackerApp.swift
+//  Weather Tracker
+//
+//  Created by shashank on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
